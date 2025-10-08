d11f2864735b3dae556e1a937086616530e27de4
apossstle <apostlebehelite@gmail.com>
2025-10-08 16:11:45 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using TubeShopBackend.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем сервисы
+builder.Services.AddControllers();
+builder.Services.AddSingleton<DataService>();
+builder.Services.AddSingleton<CartService>();
+
+// НАСТРОЙКА CORS ДЛЯ TELEGRAM MINI APP
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowTelegram", policy =>
+    {
+        policy.AllowAnyOrigin()  // Разрешаем все источники для разработки
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// ИСПОЛЬЗУЕМ CORS ПРАВИЛЬНО
+app.UseCors("AllowTelegram");
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
